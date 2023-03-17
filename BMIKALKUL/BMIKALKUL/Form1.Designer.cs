23636aa1626c960dd6081a3c9dd32e6490c07e35
L1F1N <118181137+L1F1N@users.noreply.github.com>
2023-03-17 09:14:12 +0300

    Add files via upload

---
+++ BMIKALKUL/BMIKALKUL/Form1.Designer.cs
@@ -0,0 +1,416 @@
+﻿namespace BMIKALKUL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rost = new System.Windows.Forms.TextBox();
+            this.ves = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.zna = new System.Windows.Forms.Label();
+            this.verh = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(246, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BMI калькулятор";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(30, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(739, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BMI калькулятор определяет находится ли вес в норме или есть отклонения";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(31, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Рост";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(31, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 22);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Вес";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(167, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "см";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(167, 319);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 22);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "кг";
+            // 
+            // rost
+            // 
+            this.rost.Location = new System.Drawing.Point(92, 283);
+            this.rost.Name = "rost";
+            this.rost.Size = new System.Drawing.Size(65, 20);
+            this.rost.TabIndex = 10;
+            // 
+            // ves
+            // 
+            this.ves.Location = new System.Drawing.Point(92, 319);
+            this.ves.Name = "ves";
+            this.ves.Size = new System.Drawing.Size(65, 20);
+            this.ves.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 45);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 45);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Cyan;
+            this.label5.Location = new System.Drawing.Point(308, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 23);
+            this.label5.TabIndex = 15;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(509, 378);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 23);
+            this.label6.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Lime;
+            this.label9.Location = new System.Drawing.Point(452, 378);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(74, 23);
+            this.label9.TabIndex = 17;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Yellow;
+            this.label10.Location = new System.Drawing.Point(509, 378);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(46, 23);
+            this.label10.TabIndex = 18;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label11.Location = new System.Drawing.Point(550, 378);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(152, 23);
+            this.label11.TabIndex = 19;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(308, 330);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(394, 45);
+            this.trackBar1.TabIndex = 20;
+            // 
+            // zna
+            // 
+            this.zna.AutoSize = true;
+            this.zna.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.zna.Location = new System.Drawing.Point(355, 296);
+            this.zna.Name = "zna";
+            this.zna.Size = new System.Drawing.Size(0, 17);
+            this.zna.TabIndex = 25;
+            // 
+            // verh
+            // 
+            this.verh.AutoSize = true;
+            this.verh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.verh.Location = new System.Drawing.Point(355, 266);
+            this.verh.Name = "verh";
+            this.verh.Size = new System.Drawing.Size(0, 17);
+            this.verh.TabIndex = 26;
+            this.verh.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(343, 417);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(78, 13);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "Недостаточно";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(509, 417);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(52, 13);
+            this.label14.TabIndex = 29;
+            this.label14.Text = "Избыток";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(613, 417);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(59, 13);
+            this.label15.TabIndex = 30;
+            this.label15.Text = "Ожирение";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(452, 417);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(41, 13);
+            this.label16.TabIndex = 28;
+            this.label16.Text = "Норма";
+            // 
+            // pb6
+            // 
+            this.pb6.BackgroundImage = global::BMIKALKUL.Properties.Resources.Screenshot_81;
+            this.pb6.Location = new System.Drawing.Point(384, 95);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(100, 168);
+            this.pb6.TabIndex = 24;
+            this.pb6.TabStop = false;
+            this.pb6.Visible = false;
+            this.pb6.Click += new System.EventHandler(this.pb6_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.BackgroundImage = global::BMIKALKUL.Properties.Resources.Screenshot_80;
+            this.pb5.Location = new System.Drawing.Point(384, 95);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(101, 168);
+            this.pb5.TabIndex = 23;
+            this.pb5.TabStop = false;
+            this.pb5.Visible = false;
+            // 
+            // pb4
+            // 
+            this.pb4.BackgroundImage = global::BMIKALKUL.Properties.Resources.Screenshot_79;
+            this.pb4.Location = new System.Drawing.Point(384, 95);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(98, 168);
+            this.pb4.TabIndex = 22;
+            this.pb4.TabStop = false;
+            this.pb4.Visible = false;
+            this.pb4.Click += new System.EventHandler(this.pb4_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.BackgroundImage = global::BMIKALKUL.Properties.Resources.Screenshot_78;
+            this.pb3.Location = new System.Drawing.Point(384, 95);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(81, 168);
+            this.pb3.TabIndex = 21;
+            this.pb3.TabStop = false;
+            this.pb3.Visible = false;
+            // 
+            // pb2
+            // 
+            this.pb2.BackgroundImage = global::BMIKALKUL.Properties.Resources.Screenshot_83;
+            this.pb2.Location = new System.Drawing.Point(158, 83);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(109, 168);
+            this.pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb2.TabIndex = 12;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.BackgroundImage = global::BMIKALKUL.Properties.Resources.Screenshot_77;
+            this.pb1.Location = new System.Drawing.Point(35, 83);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(104, 168);
+            this.pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pb1.TabIndex = 2;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.verh);
+            this.Controls.Add(this.zna);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.ves);
+            this.Controls.Add(this.rost);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BMI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox rost;
+        private System.Windows.Forms.TextBox ves;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.Label zna;
+        private System.Windows.Forms.Label verh;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.PictureBox pb1;
+    }
+}
+
---
+++ BMIKALKUL/BMIKALKUL/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMIKALKUL
+{
+    public partial class Form1 : Form
+    {
+        float i;
+        float z, x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pb1.BorderStyle = BorderStyle.FixedSingle;
+            pb1.BackColor = Color.LightGray;
+
+            pb2.BorderStyle = BorderStyle.None;
+            pb2.BackColor = Color.WhiteSmoke;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rost.Text = null;
+            ves.Text = null;
+            verh.Text = null;
+            zna.Text = null;
+            trackBar1.Value = 0;
+            pb1.BorderStyle = BorderStyle.None;
+            pb1.BackColor = Color.WhiteSmoke;
+            pb2.BorderStyle = BorderStyle.None;
+            pb2.BackColor = Color.WhiteSmoke;
+            pb3.Visible = false;
+            pb4.Visible = true;
+            pb5.Visible = false;
+            pb6.Visible = false;
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+            pb2.BorderStyle = BorderStyle.FixedSingle;
+            pb2.BackColor = Color.LightGray;
+
+            pb1.BorderStyle = BorderStyle.None;
+            pb1.BackColor = Color.WhiteSmoke;
+        }
+
+        private void pb6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = float.Parse(rost.Text);
+            z = float.Parse(ves.Text);
+            x /= 100;
+            i = z / (x * x);
+            verh.Text = i.ToString();
+            trackBar1.Value = (int)i;
+            pb3.Visible = false;
+            pb4.Visible = false;
+            pb5.Visible = false;
+            pb6.Visible = false;
+
+            if (i < 18.5)
+            {
+                pb3.Visible = true;
+                zna.Text = "Недостаточный вес";
+            }
+
+            if (i >= 18.5 && i <= 24.9)
+            {
+                pb4.Visible = true;
+                zna.Text = "Здоровый вес";
+            }
+
+
+            if (i >= 25 && i < 29.9)
+            {
+                pb5.Visible = true;
+                zna.Text = "Избыточный вес";
+            }
+
+
+            if (i >= 30)
+            {
+                pb6.Visible = true;
+                zna.Text = "Ожирение";
+            }
+        }
+    }
+}
